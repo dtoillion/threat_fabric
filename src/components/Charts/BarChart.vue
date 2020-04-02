3bc26cb14007855d80838726f770323b7a35bbf7
dtoillion <toillion@gmail.com>
2020-04-02 19:03:17 +0200

    Start scaffolding some chart components

---
+++ src/components/Charts/BarChart.vue
@@ -0,0 +1,44 @@
+<template>
+  <Echarts :option="chartOptions" :resizable="true" />
+</template>
+
+<script>
+import Echarts from 'vue-echarts-v3/src/full.js';
+
+export default {
+  name: 'BarChart',
+  components: {
+    Echarts,
+  },
+  data() {
+    return {
+      chartOptions: {
+        backgroundColor: '#2c343c',
+        textStyle: {
+          color: '#F1F1F1',
+        },
+        title: {
+          text: 'Customized Pie',
+          left: 'center',
+          top: 20,
+          textStyle: {
+            color: '#F1F1F1',
+          },
+        },
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        },
+        yAxis: {
+          type: 'value',
+
+        },
+        series: [{
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: 'bar',
+        }],
+      },
+    };
+  },
+};
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,40 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <div class="row justify-center">
+      <q-card
+        class="col-12 col-sm-6 col-lg-4 q-gutter-xl"
+        flat
+        square
+      >
+        <q-card-section class="echarts q-pa-none">
+          <PieChart style="min-height: 350px;" />
+        </q-card-section>
+      </q-card>
+      <q-card
+        class="col-12 col-sm-6 col-lg-4 q-gutter-xl"
+        flat
+        square
+      >
+        <q-card-section class="echarts q-pa-none">
+          <BarChart style="min-height: 350px;" />
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
 <script>
+import PieChart from 'components/Charts/PieChart';
+import BarChart from 'components/Charts/BarChart';
+
 export default {
-  name: 'PageIndex',
+  name: 'Index',
+  data() {
+    return {};
+  },
+  components: {
+    PieChart,
+    BarChart,
+  },
 };
 </script>
